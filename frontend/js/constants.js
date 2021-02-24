6b9df045972569d8d6b9b7375bc6eae199d89102
potatoe <aprilgvguevara@gmail.com>
2021-02-25 07:24:44 +0800

    [FE] Modify ChartComponent.js

---
+++ frontend/js/constants.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "RGB_BLUE": 'rgb(132, 99, 255)',
+  "RGB_GREEN": 'rgb(132, 255, 99)',
+  "RGB_RED": 'rgb(255, 99, 132)'
+};
--- frontend/src/components/ChartComponent.js
+++ frontend/src/components/ChartComponent.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import { Line } from '@reactchartjs/react-chart.js';
+
+import { RGB_GREEN, RGB_BLUE, RGB_RED } from '../../js/constants';
+
+const data = {
+  labels: ["2020-07-13", "2020-07-14", "2020-07-15", "2020-07-16", "2020-07-17", "2020-07-18"],
+  datasets: [
+    {
+      label: 'Confirmed',
+      data: ["20371", "20459", "20976", "21440", "21748", "22067"],
+      fill: false,
+      backgroundColor: RGB_BLUE,
+      borderColor: RGB_BLUE,
+    },
+    {
+      label: 'Recovered',
+      data: ["2037", "2045", "2097", "2144", "2174", "2206"],
+      fill: false,
+      backgroundColor: RGB_GREEN,
+      borderColor: RGB_GREEN,
+    },
+    {
+      label: 'Deceased',
+      data: ["1599", "1603", "1614", "1643", "1660", "1773"],
+      fill: false,
+      backgroundColor: RGB_RED,
+      borderColor: RGB_RED,
+    }
+  ],
+}
+
+const options = {
+  responsive: true
+  // maintainAspectRatio: false,
+  // scales: {
+  //   yAxes: [
+  //     {
+  //       ticks: {
+  //         beginAtZero: true,
+  //       },
+  //     },
+  //   ],
+  // },
+}
 
 const Chart = () => {
   return (
-    <div>
-      <h1> Chart </h1>
+    <div className="card mt-3">
+      <div className="card-header text-center">
+        Cumulative Summary Chart
+      </div>
+      <div className="card-body row">
+        <Line data={data} options={options} />
+      </div>
     </div>
   );
 };
