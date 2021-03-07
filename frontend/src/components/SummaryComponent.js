ea785b3dda191582d5a7fb4e6274fbb908c7150d
potatoe <aprilgvguevara@gmail.com>
2021-03-07 17:18:23 +0800

    [FE] Minor update -> syntax and spacing modifications

--- frontend/src/components/SummaryComponent.js
+++ frontend/src/components/SummaryComponent.js
@@ -1,12 +1,33 @@
 import React from 'react';
 
-import { DATA_URL } from '../js/constants'; 
+import { DATA_URL } from '../js/constants';
+
+const CARDS_DATA_LIST = [
+  {
+    dataName: 'cases',
+    statLabel: 'Confirmed',
+    faIconClass: 'fa-user-plus',
+    bgColorClass: 'bg-primary'
+  },
+  {
+    dataName: 'recovered',
+    statLabel: 'Recovered',
+    faIconClass: 'fa-medkit',
+    bgColorClass: 'bg-success'
+  },
+  {
+    dataName: 'died',
+    statLabel: 'Deceased',
+    faIconClass: 'fa-user-times',
+    bgColorClass: 'bg-danger'
+  }
+];
 
 const CardItem = ({statLabel, statNum, faIconClass, bgColorClass}) => {
   let iconClasses = ['fa', faIconClass, 'fa-lg'];
   let footerClasses = 'card-footer text-white small py-2 ' + bgColorClass;
   return (
-    <div className="col-12 col-md-4 mb-3 mb-lg-0">
+    <div className="col-12 col-md-4 mt-3">
       <div className="card">
         <div className="card-body">
           <div className="row my-auto font-weight-bold h3">
@@ -30,30 +51,10 @@ const CardItem = ({statLabel, statNum, faIconClass, bgColorClass}) => {
 
 const Summary = (props) => {
   let { latestData, latestUpdateDate } = props;
-  let cardsDataList = [
-    {
-      "statLabel": 'CONFIRMED',
-      "statNum": latestData ? latestData.cases : 0,
-      "faIconClass": 'fa-user-plus',
-      "bgColorClass": 'bg-primary'
-    },
-    {
-      "statLabel": 'RECOVERED',
-      "statNum": latestData ? latestData.recovered : 0,
-      "faIconClass": 'fa-medkit',
-      "bgColorClass": 'bg-success'
-    },
-    {
-      "statLabel": 'DECEASED',
-      "statNum": latestData ? latestData.died : 0,
-      "faIconClass": 'fa-user-times',
-      "bgColorClass": 'bg-danger'
-    }
-  ];
 
   return (
     <React.Fragment>
-      <div className="card mb-3">
+      <div className="card">
         <div className="card-header font-weight-bold h3 bg-white">
           {console.log(latestData)}
           OVERVIEW OF CASES {latestData.date ? 'AS OF ' + latestData.date : ''}
@@ -65,11 +66,11 @@ const Summary = (props) => {
       </div>
 
       <div className="row justify-content-center">
-        {cardsDataList.map(data => {
+        {CARDS_DATA_LIST.map(data => {
           return (
             <CardItem key={data.statLabel}
                       statLabel={data.statLabel}
-                      statNum={data.statNum}
+                      statNum={latestData ? latestData[data.dataName] : 0}
                       faIconClass={data.faIconClass}
                       bgColorClass={data.bgColorClass} />
           );
