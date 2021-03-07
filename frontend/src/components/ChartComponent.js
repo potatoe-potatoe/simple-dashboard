241c14a5fa32f9e1c009d4dd78e0f2ea6e0e970f
potatoe <aprilgvguevara@gmail.com>
2021-03-07 17:21:41 +0800

    [FE] Changed overview chart from daily to cumulative + upgrades

--- frontend/src/components/ChartComponent.js
+++ frontend/src/components/ChartComponent.js
@@ -1,51 +1,118 @@
 import React from 'react';
-import { Line } from '@reactchartjs/react-chart.js';
+import { Line } from 'react-chartjs-2';
 
-import { RGB_GREEN, RGB_BLUE, RGB_RED } from '../js/constants';
+import { CHART_COLORS } from '../js/constants';
+import { getMMMDDString } from '../js/utils';
 
-const options = {
-  responsive: true
-}
+// Custom ChartJS plugin for dynamic vertical line.
+const verticalLinePlugin = {
+  id: 'verticalLinePlugin',
+  afterDatasetDraw: chart => {
+    if (chart.tooltip._active && chart.tooltip._active.length) {
+      let activePoint = chart.tooltip._active[0],
+          ctx = chart.ctx,
+          tooltipX = activePoint.tooltipPosition().x,
+          topY = chart.scales['y-axis-0'].top,
+          bottomY = chart.scales['y-axis-0'].bottom;
+
+      // Set line config.
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = '#777';
+
+      // Draw the line.
+      ctx.save();
+      ctx.beginPath();
+      ctx.moveTo(tooltipX, topY);
+      ctx.lineTo(tooltipX, bottomY);
+      ctx.stroke();
+      ctx.restore();
+    }
+  }
+};
 
 const Chart = (props) => {
-  const chartData = props.data;
-  const displayData = {
-    labels: Array.isArray(chartData) ? chartData.map(dailyData => dailyData.date) : [],
+  let chartData = props.data;
+
+  // ----- Data processing
+  // TODO: Look at using PropTypes for checking props.
+  let chartDataLength = Array.isArray(chartData) ? chartData.length : 0;
+  let dates = [],
+      dataCases = [],
+      dataRecovered = [],
+      dataDied = [];
+
+  const IDX_FIRST = 0;
+  if (chartDataLength) {
+    dates = [ getMMMDDString(chartData[IDX_FIRST].date) ];
+    dataCases = [ chartData[IDX_FIRST].cases ];
+    dataRecovered = [ chartData[IDX_FIRST].recovered ];
+    dataDied = [ chartData[IDX_FIRST].died ];
+  }
+
+  for (var i = 1; i < chartDataLength; i++) {
+    let dailyData = chartData[i];
+    dates.push(getMMMDDString(dailyData.date))
+    dataCases.push(dataCases[i-1] + dailyData.cases);
+    dataRecovered.push(dataRecovered[i-1] + dailyData.recovered);
+    dataDied.push(dataDied[i-1] + dailyData.died);
+  }
+
+  // ----- Chart options/set-up
+  const options = {
+    legend: {
+      align: 'end'  
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false
+    }
+  };
+
+  const commonDatasetOpts = {
+    fill: true,
+    borderWidth: 5,
+    pointRadius: 0
+  };
+
+  let displayData = {
+    labels: dates,
     datasets: [
       {
         label: 'Confirmed',
-        data: Array.isArray(chartData) ? chartData.map(dailyData => dailyData.cases) : [],
-        fill: false,
-        backgroundColor: RGB_BLUE,
-        borderColor: RGB_BLUE,
+        data: dataCases,
+        borderColor: CHART_COLORS.BLUE.REGULAR,
+        backgroundColor: CHART_COLORS.BLUE.ALPHA,
+        ... commonDatasetOpts
       },
       {
         label: 'Recovered',
-        data: Array.isArray(chartData) ? chartData.map(dailyData => dailyData.recovered) : [],
-        fill: false,
-        backgroundColor: RGB_GREEN,
-        borderColor: RGB_GREEN,
+        data: dataRecovered,
+        borderColor: CHART_COLORS.GREEN.REGULAR,
+        backgroundColor: CHART_COLORS.GREEN.ALPHA,
+        ... commonDatasetOpts
       },
       {
         label: 'Deceased',
-        data: Array.isArray(chartData) ? chartData.map(dailyData => dailyData.died) : [],
-        fill: false,
-        backgroundColor: RGB_RED,
-        borderColor: RGB_RED,
+        data: dataDied,
+        borderColor: CHART_COLORS.RED.REGULAR,
+        backgroundColor: CHART_COLORS.RED.ALPHA,
+        ... commonDatasetOpts
       }
-    ],
+    ]
   };
 
+  // ----- Render
   return (
     <div className="card mt-3">
       <div className="card-header text-center font-weight-bold">
-        Case Overview Time Series Graph
+        Cumulative Data Overview
       </div>
-      <div className="card-body row">
-        <Line data={displayData} options={options} />
+
+      <div className="card-body">
+        <Line data={displayData} options={options} plugins={[verticalLinePlugin]} />
       </div>
     </div>
   );
-};
+}
 
 export default Chart;
--- frontend/src/js/constants.js
+++ frontend/src/js/constants.js
@@ -1,6 +1,19 @@
+const DEFAULT_ALPHA = 0.2;
+
 module.exports = {
-  "RGB_BLUE": 'rgb(2, 117, 216)',
-  "RGB_GREEN": 'rgb(92, 184, 92)',
-  "RGB_RED": 'rgb(217, 83, 79)',
-  "DATA_URL": 'https://covid19-api-philippines.herokuapp.com/api/timeline'
+  CHART_COLORS: {
+    BLUE: {
+      REGULAR: 'rgb(2, 117, 216)',
+      ALPHA: `rgba(2, 117, 216, ${DEFAULT_ALPHA})`
+    },
+    GREEN: {
+      REGULAR: 'rgb(92, 184, 92)',
+      ALPHA: `rgba(92, 184, 92, ${DEFAULT_ALPHA})`
+    },
+    RED: {
+      REGULAR: 'rgb(217, 83, 79)',
+      ALPHA: `rgba(217, 83, 79, ${DEFAULT_ALPHA})`
+    }, 
+  },
+  DATA_URL: 'https://covid19-api-philippines.herokuapp.com/api/timeline'
 };
